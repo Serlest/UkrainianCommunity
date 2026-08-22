c4dc680b3f7aeb3d6fdeac4f663feb930b605391
Philipp Timofeev <77774411+Serlest@users.noreply.github.com>
2026-08-22 09:26:52 +0200

    Move account deletion to trusted backend

--- functions/src/firebase/admin.ts
+++ functions/src/firebase/admin.ts
@@ -1,8 +1,10 @@
 import { getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from "firebase-admin/storage";
 
 const app = getApps().length === 0 ? initializeApp() : getApps()[0];
 
 export const adminAuth = getAuth(app);
 export const db = getFirestore(app);
+export const adminStorage = getStorage(app);
